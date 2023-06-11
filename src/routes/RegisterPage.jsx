dd43bc2bd43a90143c477c0a9fd84ed7a8f75322
CLAUDIO <ccoding0@gmail.com>
2023-06-11 17:43:39 +0100

    removed checkbox

--- src/routes/RegisterPage.jsx
+++ src/routes/RegisterPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Form, Input, Button, Checkbox, Typography } from "antd";
+import { Form, Input, Button, Typography } from "antd";
 
 const { Title } = Typography;
 
